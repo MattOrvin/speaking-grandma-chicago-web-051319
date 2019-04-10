a6a6781ebf454b6325cf4f6cd854c5252aa8f085
Matt Orvin <matthew.orvin@gmail.com>
2019-04-10 20:16:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase.upcase == false
     "HUH?! SPEAK UP, SONNY!"
-  elsif
+  else
     "NO, NOT SINCE 1938!"
-  else phrase = "I LOVE YOU GRANDMA!"
+  elsif phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
 end
 end
